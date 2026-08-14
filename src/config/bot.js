b448d71c764057adefeca430851505e7da699d1f
simonh7807-sys <simonh7807@gmail.com>
2026-08-14 15:40:20 +0100

    Change bot status from 'Watching Comms' to 'Watching'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Watching Comms",     // this is what people actually see
+        state: "Watching",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
